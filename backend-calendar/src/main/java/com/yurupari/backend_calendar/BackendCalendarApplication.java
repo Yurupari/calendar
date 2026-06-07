27399e073eacb91628d35f60d42d1019e28d6b7d
Yurupari <cristhianmarin15@gmail.com>
2026-06-07 15:24:09 +0200

    Initial commit

---
+++ backend-calendar/src/main/java/com/yurupari/backend_calendar/BackendCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.yurupari.backend_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCalendarApplication.class, args);
+	}
+
+}
---
+++ backend-calendar/src/test/java/com/yurupari/backend_calendar/BackendCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurupari.backend_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
