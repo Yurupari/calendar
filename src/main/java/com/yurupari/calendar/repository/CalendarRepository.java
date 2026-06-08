6ce692ce9845c2eb9a98e386d9ee6f79d25b198f
Yurupari <cristhianmarin15@gmail.com>
2026-06-08 17:46:56 +0200

    cristhian: added queries

--- src/main/java/com/yurupari/calendar/repository/CalendarRepository.java
+++ src/main/java/com/yurupari/calendar/repository/CalendarRepository.java
@@ -4,6 +4,10 @@ import com.yurupari.calendar.model.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+
+    Optional<Calendar> findByUserId(Long userId);
 }
